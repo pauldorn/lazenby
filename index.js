4dd1af7001af4b440e3008ac7ded48aeb50ab2d2
Paul Dorn <pauldorn@gmail.com>
2014-07-07 15:25:22 -0500

    Node express server is working

--- index.js
+++ index.js
@@ -8,3 +8,18 @@
 
 var requirejs = require('requirejs'); // AMD style module loading
 
+requirejs(['express', 'morgan'], function(express, morgan){
+
+	var app = express();
+	app.use(morgan('dev'));
+	app.use(express.static('/js/ko', __dirname + '/node_modules/knockout/build/output/'));
+	app.use(express.static(__dirname + '/public'));
+
+//	app.route('/', function(req, res){
+//		res.send('Hello World');
+//	});
+
+	app.listen(3000);
+	console.log('Express started on port 3000');
+
+});
