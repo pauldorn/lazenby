c7bb063e08f3359818861ac538c648b394c0216c
pauldorn <pauldorn@gmail.com>
2014-07-07 18:17:41 -0500

    Add tests directory

--- index.js
+++ index.js
@@ -8,13 +8,34 @@
 
 var requirejs = require('requirejs'); // AMD style module loading
 
-requirejs(['express', 'morgan'], function(express, morgan){
+requirejs(['express', 'morgan', 'path', 'fs'], function(express, morgan, path, fs){
 
 	var app = express();
 	app.use(morgan('dev'));
-	app.use(express.static('/js/ko', __dirname + '/node_modules/knockout/build/output/'));
-	app.use(express.static(__dirname + '/public'));
+	app.use(express.static('/js/ko', path.join(__dirname,'node_modules' , 'knockout','build','output')));
+	app.use(express.static( path.join(__dirname, 'public')));
 
+    app.get('/getTestYears', function(req, res){
+        fs.readdir('tests', function(err, files){
+            if(err) {
+                res.json({ error: err});
+            } else {
+                res.json({ data: files });
+            }
+
+        });
+    });
+
+    app.get('/getTestMonths', function(req, res){
+        fs.readdir(path.join('tests', req.query.testYear), function(err, files){
+            if(err) {
+                res.json({ error: err});
+            } else {
+                res.json({ data: files });
+            }
+
+        });
+    });
 //	app.route('/', function(req, res){
 //		res.send('Hello World');
 //	});
